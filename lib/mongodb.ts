2aeea4b902c6a81ecb69c478f5735a5b50cf496c
aliaamir-stack <shanzayaijazkhan@gmail.com>
2025-07-14 17:45:05 +0500

    fix: use typed let for client to pass eslint

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -7,8 +7,9 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI
 const options = {}
 
-let client
+let client: MongoClient
 let clientPromise: Promise<MongoClient>
+
 
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
